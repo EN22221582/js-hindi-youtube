ef87fcc6e4c5cab3cb14ccc00481a0cde03adaf9
EN22221582 <ishwarithakre962@gmail.com>
2025-06-29 17:10:25 +0000

    data_types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,43 @@
+"use strict";      // treat all js code as newer version of js in whole its file ......  But nowadays no need to write this due to advancement in js it already treated code as newer version
+//alert(3+3)       //will give error beacause we are using nodejs , not browser .... It gets used in browser It will give pop up 6 as output of this line we used it in js lectures of Apna clg 
+console.log(3
+    +
+    3)             // Dont write code lines like this it will give ans correct but readability gets reduced ... CODE READABILITY SHOULD BE HIGH
+    console.log("ish");
+
+
+ let name="ishwari"           //data type = string
+ let age=18                   //data type = number
+ let isLoggedIn=false         //data type = boolean
+ let temp=null                //data type = OBJECT ...... not null   {this null means not zero it not mean temp=0 it means no value is there for temp it is empty}
+ let state;                   //data type = undefined ...... {not defined yet}
+
+
+ // THERE ARE MAIN 2 TYPES OF DATA TYPES: 
+
+ //********1ST PRIMITIVE : **********
+ //  number  => range 2 to power 53
+ // bigint   => use for large integer having range> that of number
+ //string    =>""
+ //boolean   =>true/false
+ //null      =>standalone value
+ //undefined
+ //symbol    => use for uniqueness of components
+
+//************* 2nd Non primitive : (reference ) *************/
+//object
+//array
+//function 
+          
+                                     //output:
+console.log(typeof "ishwari");       //string
+console.log(typeof age);             //number
+console.log(typeof null)             //object
+console.log(typeof undefined);       //undefined
+
+
+//*******you can read documentation of js on mdn or acmyscript *******//
+
+
+
+                  
